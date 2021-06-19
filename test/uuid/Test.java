857b7ca0adb5bb0396f435763d75f0715f2c093b
Yasumasa Suenaga <yasuenag@gmail.com>
2021-06-19 11:15:21 +0900

    Refactor UUID test

--- test/uuid/Test.java
+++ test/uuid/Test.java
@@ -1,17 +1,46 @@
 import java.util.*;
+import java.util.concurrent.*;
 
-public class Test implements Runnable{
+public class Test implements Callable<Set<UUID>>{
 
-  public void run(){
-    for(int i = 0; i < 1_000_000; i++){
-      UUID.randomUUID();
+  private final static int NUM_OF_SETS    = 100_000;
+  private final static int NUM_OF_THREADS = 100;
+
+  @Override
+  public Set<UUID> call(){
+    Set<UUID> uuids = new HashSet<>();
+
+    for(int i = 0; i < NUM_OF_SETS; i++){
+      uuids.add(UUID.randomUUID());
     }
+
+    return uuids;
   }
 
   public static void main(String[] args) throws Exception{
-    for(int i = 0; i < 100; i++){
-      (new Thread(new Test())).start();
+    List<Callable<Set<UUID>>> tasks = new ArrayList<>();
+    for(int i = 0; i < NUM_OF_THREADS; i++){
+      tasks.add(new Test());
     }
+
+    var uuids = new HashSet<UUID>();
+    ExecutorService tp = Executors.newFixedThreadPool(tasks.size());
+    long start = System.currentTimeMillis();
+    for(var future : tp.invokeAll(tasks)){
+      uuids.addAll(future.get());
+    }
+    long end = System.currentTimeMillis();
+
+    int expectedResults = NUM_OF_SETS * NUM_OF_THREADS;
+    if(uuids.size() == expectedResults){
+      System.out.println("Test OK!");
+    }
+    else{
+      throw new RuntimeException("Expected: " + expectedResults + ", actual: " + uuids.size());
+    }
+
+    tp.shutdownNow();
+    System.out.println("Elapsed Time (ms): " + Long.toString(end - start));
   }
 
 }
--- test/uuid/test.sh
+++ test/uuid/test.sh
@@ -2,7 +2,9 @@
 
 DISTDIR=../../dist
 export CLASSPATH=.:$DISTDIR/hwrand.jar
+
+# Please comment out if you want to measure performance of default SecureRandom.
 SECURITY_OPTS=-Djava.security.properties=java.security
 
 $JAVA_HOME/bin/javac Test.java
-$JAVA_HOME/bin/java -Djava.library.path=$DISTDIR $SECURITY_OPTS -XX:StartFlightRecording=filename=uuid.jfr Test
+$JAVA_HOME/bin/java -Djava.library.path=$DISTDIR $SECURITY_OPTS -Xms8g -Xmx8g -XX:StartFlightRecording=filename=uuid.jfr Test
