15a362e1c45fb1939ac8d2ad5610a1ed5e740bcd
Yasumasa Suenaga <yasuenag@gmail.com>
2021-06-19 23:48:33 +0900

    Add benchmark

---
+++ benchmark/README.md
@@ -0,0 +1,23 @@
+# HWRandベンチマーク
+
+NativePRNGNonBlocking、DRBG、X86RdRand、X86RdSeedのパフォーマンスをJMHで測定します。シングルスレッド性能と100スレッド同時実行の2パターンを測定します。
+
+## 必要なもの
+
+* JDK 16
+* Maven
+* HWRand
+    * **maven installされている必要あり**
+    * [pom.xml](pom.xml)で依存設定しています
+
+## 動かし方
+
+```
+$ java -Djava.library.path=<libhwrand.so配置先> -jar target/hwrand-benchmark-1.0.0.jar
+```
+
+## 注意
+
+* Linuxのみで動きます
+* -Xmxで8GB設定しています
+* ベンチマーク内部でEpsilon GCを設定しています
---
+++ benchmark/src/main/java/com/yasuenag/hwrand/benchmark/RandomBenchmark.java
@@ -0,0 +1,66 @@
+package com.yasuenag.hwrand.benchmark;
+
+import java.security.*;
+import java.util.concurrent.*;
+
+import com.yasuenag.hwrand.x86.*;
+import org.openjdk.jmh.annotations.*;
+
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.Throughput)
+@Fork(value = 1, jvmArgsAppend = {"-Xms8g", "-Xmx8g", "-XX:+UnlockExperimentalVMOptions", "-XX:+UseEpsilonGC", "-XX:+AlwaysPreTouch"})
+@Warmup(iterations = 1, time = 3, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 3, time = 10, timeUnit = TimeUnit.SECONDS)
+public class RandomBenchmark{
+
+  private SecureRandom rand;
+
+  // WARNING: NativePRNGBlocking might be hang if entropy is exhausted.
+  @Param({/* "NativePRNGBlocking", */
+          "NativePRNGNonBlocking", "DRBG", "X86RdRand", "X86RdSeed"})
+  private String algo;
+
+  static{
+    Security.addProvider(new HWRandX86Provider());
+  }
+
+  @Setup
+  public void setup(){
+    try{
+      rand = SecureRandom.getInstance(algo);
+    }
+    catch(NoSuchAlgorithmException e){
+      throw new RuntimeException(e);
+    }
+  }
+
+  @State(Scope.Thread)
+  public static class RandomHolder{
+
+    @Param("16") // 128bit
+    private int bytes;
+
+    public byte[] randomVal;
+
+    @Setup
+    public void setup(){
+      randomVal = new byte[bytes];
+    }
+  }
+
+  @Benchmark
+  @Threads(1)
+  public byte[] fillRandom(RandomHolder holder){
+    rand.nextBytes(holder.randomVal);
+    return holder.randomVal;
+  }
+
+  @Benchmark
+  @Threads(100)
+  public byte[] fillRandomInMT(RandomHolder holder){
+    rand.nextBytes(holder.randomVal);
+    return holder.randomVal;
+  }
+
+}
